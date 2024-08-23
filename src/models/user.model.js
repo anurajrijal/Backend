348325820b2e4c1ffcd6ef5db8fd8ae6f1bb416e
anurajrijal <anurajrijla@gmail.com>
2024-08-23 23:39:27 +0545

    register controller

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,5 @@
 import mongoose,{Schema} from "mongoose";
-import { JsonWebTokenError } from "jsonwebtoken";
+import  jwt  from "jsonwebtoken";
 import bcrypt from "bcrypt";
 
 const userSchema = new Schema(
@@ -19,7 +19,7 @@ const userSchema = new Schema(
             lowercase:true,
             trim:true,
         },
-        fullname:{
+        fullName:{
             type:String,
             required: [true,'Fullname is required'],
             lowercase:true,
@@ -50,7 +50,7 @@ const userSchema = new Schema(
 )
 userSchema.pre("save",async function (next) {
     if(!this.isModified("password")) return next();
-    this.password = bcrypt.hash(this.password, 10)
+    this.password = await bcrypt.hash(this.password, 10)
     next()
 })
 
@@ -58,11 +58,11 @@ userSchema.methods.isPasswordCorrect= async function (password) {
    return await bcrypt.compare(password, this.password)
 }
 userSchema.methods.generateAccessToken = function(){
-   return  JsonWebToken.sign({
+   return  jwt.sign({
         _id:this._id,
         email:this.email,
         username:this.username,
-        fullname:this.fullname
+        fullName:this.fullName
     },
     process.env.ACCESS_TOKEN_SECRET,
         {
@@ -71,7 +71,7 @@ userSchema.methods.generateAccessToken = function(){
     )
 }
 userSchema.methods.generateRefreshToken = function(){
-    return  JsonWebToken.sign({
+    return  jwt.sign({
         _id:this._id,
 
     },
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,5 +1,5 @@
 import {v2 as cloudinary} from 'cloudinary';
-import { log } from 'console';
+
 import fs from "fs"; //file system by default in nodejs to write read etc files
 
 
@@ -16,11 +16,12 @@ import fs from "fs"; //file system by default in nodejs to write read etc files
             if(!localFilePath)return null
             //upload the file on cloudinary
            const response = await cloudinary.uploader.upload(localFilePath,{
-                resource_type:'auto'
+                resource_type:"auto"
             })  
             //file has been uploaded successfully
-            console.log('file is uploaded on cloudinary',response.url);
-            return response          
+            // console.log('file is uploaded on cloudinary',response.url);
+            fs.unlinkSync(localFilePath)
+            return response;          
 
         }catch(error){
             fs.unlinkSync(localFilePath) //remove the locally saved temporary file as the upload operation got failed
